a5491cb811440d8c598487e251c968c223fcec16
RuiGuo12345 <145431027+RuiGuo12345@users.noreply.github.com>
2023-10-12 18:00:23 +1100

    Add files via upload
    data visualization 2 (for critique)

---
+++ js/FIT3179_data_visulation2.js
@@ -0,0 +1,19 @@
+var vg1 = "js/G1_map.json";
+vegaEmbed('#gragh1', vg1).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg2 = "js/FIT3179_w10_hw.vg.json";
+vegaEmbed('#graph2', vg2).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg3 = "js/G3_AreaChart.json";
+vegaEmbed('#graph3', vg3).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg4 = "js/G4_rank.json";
+vegaEmbed('#graph4', vg4).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
